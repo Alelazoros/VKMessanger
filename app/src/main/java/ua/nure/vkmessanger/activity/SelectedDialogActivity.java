a88d1b26c04e202c141dcb92d2972592d2ed735a
Polant <anton.poltoratskii@gmail.com>
2016-03-30 18:44:00 +0300

    delete FloatingActionButton from SelectedDialogActivity

--- app/src/main/java/ua/nure/vkmessanger/activity/SelectedDialogActivity.java
+++ app/src/main/java/ua/nure/vkmessanger/activity/SelectedDialogActivity.java
@@ -50,7 +50,6 @@ public class SelectedDialogActivity extends AppCompatActivity {
 
         getDataFromIntent(getIntent());
         initToolbar();
-        initFAB();
         initRecyclerView();
         loadDialogWithSelectedUser(userId);
     }
@@ -62,17 +61,6 @@ public class SelectedDialogActivity extends AppCompatActivity {
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    private void initFAB() {
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     private void initRecyclerView() {
