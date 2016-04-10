1439c65324fb3dce070d0d77ae7a2d19e33686f9
Polant <anton.poltoratskii@gmail.com>
2016-04-10 13:47:15 +0300

    implement base logic for read wall-posts in SelectedDialogRecyclerAdapter for RecyclerView

--- app/src/main/java/ua/nure/vkmessanger/adapter/SelectedDialogRecyclerAdapter.java
+++ app/src/main/java/ua/nure/vkmessanger/adapter/SelectedDialogRecyclerAdapter.java
@@ -10,15 +10,19 @@ import android.widget.TextView;
 import java.util.List;
 
 import ua.nure.vkmessanger.R;
+import ua.nure.vkmessanger.model.Attachment;
 import ua.nure.vkmessanger.model.Message;
+import ua.nure.vkmessanger.model.WallPost;
 
 /**
  * Адаптер для списка сообщений в выбранном диалоге.
  */
 public class SelectedDialogRecyclerAdapter extends RecyclerView.Adapter<SelectedDialogRecyclerAdapter.SelectedDialogViewHolder> {
 
-    private static final int MESSAGE_FROM_USER_TYPE = 1;
-    private static final int MESSAGE_TO_USER_TYPE = 2;
+    private static final int MESSAGE_FROM_USER_TYPE = R.layout.dialog_message_from_user_layout;
+    private static final int MESSAGE_TO_USER_TYPE = R.layout.dialog_message_to_user_layout;
+    private static final int WALL_POST_FROM_USER_TYPE = R.layout.dialog_message_wall_post_from_user;
+    private static final int WALL_POST_TO_USER_TYPE = R.layout.dialog_message_wall_post_to_user;
 
     private LayoutInflater mInflater;
 
@@ -40,22 +44,30 @@ public class SelectedDialogRecyclerAdapter extends RecyclerView.Adapter<Selected
 
     @Override
     public int getItemViewType(int position) {
-        return mMessageList.get(position).isFromMe() ? MESSAGE_FROM_USER_TYPE : MESSAGE_TO_USER_TYPE;
+        if (mMessageList.get(position).isFromMe()){
+            return mMessageList.get(position).getAttachments() == null ? MESSAGE_FROM_USER_TYPE : WALL_POST_FROM_USER_TYPE;
+        }else {
+            return mMessageList.get(position).getAttachments() == null ? MESSAGE_TO_USER_TYPE : WALL_POST_TO_USER_TYPE;
+        }
     }
 
     @Override
     public SelectedDialogViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        int layout = viewType == MESSAGE_FROM_USER_TYPE ?
-                R.layout.dialog_message_from_user_layout : R.layout.dialog_message_to_user_layout;
-
-        View view = mInflater.inflate(layout, parent, false);
+        View view = mInflater.inflate(viewType, parent, false);
         return new SelectedDialogViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(SelectedDialogViewHolder holder, int position) {
         if (mMessageList != null) {
-            holder.messageTextView.setText(mMessageList.get(position).getMessageBody());
+            holder.messageTV.setText(mMessageList.get(position).getMessageBody());
+
+            Attachment[] attachments = mMessageList.get(position).getAttachments();
+
+            //TODO: пока что обрабатываю только записи на стене.
+            if (attachments != null && attachments[0] != null && attachments[0].getType().equals(Attachment.TYPE_WALL_POST)){
+                holder.attachmentWallPostTitleTV.setText(((WallPost)attachments[0].getBody()).getText());
+            }
 
             if (position == getItemCount() - 1) {
                 //Если пользователь доскроллил до конца RecyclerView, то надо подгрузить еще сообщения.
@@ -77,11 +89,14 @@ public class SelectedDialogRecyclerAdapter extends RecyclerView.Adapter<Selected
 
     static class SelectedDialogViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView messageTextView;
+        private TextView messageTV;
+
+        private TextView attachmentWallPostTitleTV;
 
         public SelectedDialogViewHolder(View itemView) {
             super(itemView);
-            messageTextView = (TextView) itemView.findViewById(R.id.messageTextView);
+            messageTV = (TextView) itemView.findViewById(R.id.messageTextView);
+            attachmentWallPostTitleTV = (TextView) itemView.findViewById(R.id.messageAttachmentWallPostTV);
         }
     }
 
